7ce07775a5419d744b7060f884533f9d9ec3734c
Cristhian Castañeda <casta0124@gmail.com>
2026-02-28 23:06:59 -0600

    ProyectoAvance6

---
+++ UberWings2/src/app/pages/drivers/drivers.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drivers',
+  imports: [],
+  templateUrl: './drivers.html',
+  styleUrl: './drivers.css',
+})
+export class Drivers {
+
+}
